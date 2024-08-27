8a7cfa25faec847539e95a1b439552fbba11af1b
Ak4shKr <akashkmr7667@gmail.com>
2024-08-27 22:41:05 +0530

    change in middleware when not find token rreturn

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,9 +2,18 @@ import jwt from "jsonwebtoken";
 
 export const authMiddleware = async (req, res, next) => {
   try {
+    console.log("Auth middleware");
     const token = req.headers["authorization"];
+    console.log("token", token);
+    if (!token) {
+      return res.status(401).send({
+        message: "Auth failed: No token provided",
+        success: false,
+      });
+    }
     jwt.verify(token, "secret", (err, decoded) => {
       if (err) {
+        console.log(err);
         return res.status(401).send({
           message: "Auth failed",
           success: false,
