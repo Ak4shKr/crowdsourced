88835d70aecd07e523ec9f19a9e9af7ed640904c
Ak4shKr <akashkmr7667@gmail.com>
2024-09-09 01:32:13 +0530

    socket io setup

--- server.js
+++ server.js
@@ -5,6 +5,32 @@ import cors from "cors";
 const app = express();
 app.use(express.json());
 
+//socketio setup
+import http from "http";
+import { Server } from "socket.io";
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", // Replace with the URL of your frontend
+    methods: ["GET", "POST"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  },
+});
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  // Example Socket.IO event
+  socket.on("chatMessage", (msg) => {
+    console.log("Message received: ", msg);
+    io.emit("chatMessage", msg); // Broadcast the message to all clients
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
 //env config
 import dotenv from "dotenv";
 dotenv.config();
@@ -12,8 +38,6 @@ const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
 //cors with origin
-// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-
 app.use(
   cors({
     origin: "http://localhost:5173", // Replace with the URL of your frontend
@@ -40,6 +64,6 @@ mongoose
   });
 
 //server connection
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log(`Server is running on port ${PORT}`);
 });
