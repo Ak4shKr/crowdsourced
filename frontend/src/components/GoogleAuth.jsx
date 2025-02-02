138da195c1f8853f061499df43fb57b7943bdcff
Ak4shKr <akashkmr7667@gmail.com>
2025-02-03 01:10:31 +0530

    xvsdfvas

--- frontend/src/components/GoogleAuth.jsx
+++ frontend/src/components/GoogleAuth.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
-import service from "../http/service";
+// import service from "../http/service";
+import axios from "axios";
 
 export const GoogleAuth = () => {
   const params = useSearchParams();
@@ -10,13 +11,25 @@ export const GoogleAuth = () => {
       // You can use this code to request the token from your backend
       const fetchGoogleToken = async () => {
         try {
-          const response = await service.post("/auth/google/callback", {
-            code,
-          });
+          const tokenResponse = await axios.post(
+            "https://oauth2.googleapis.com/token",
+            {
+              code,
+              client_id:
+                "221664733783-9lnk0s9559f85cd5aot6fg76uao2jtri.apps.googleusercontent.com",
+              client_secret: "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",
+              redirect_uri: `https://crowdsourced.vercel.app/google-auth`,
+              grant_type: "authorization_code",
+            }
+          );
+
+          console.log("tokenResponse", tokenResponse);
+          // const { access_token } = tokenResponse.data;
+
           // Assuming your backend sends the JWT token and user details
-          console.log("responce from token oauth", response);
-          localStorage.setItem("token", response.data.token);
-          localStorage.setItem("user", JSON.stringify(response.data.user));
+          console.log("responce from token oauth", tokenResponse);
+          localStorage.setItem("token", tokenResponse.data.token);
+          // localStorage.setItem("user", JSON.stringify(response.data.user));
           window.location.href = "/"; // Redirect to the home page or desired page after login
         } catch (error) {
           console.error("Error during token exchange:", error);
